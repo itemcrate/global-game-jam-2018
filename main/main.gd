dc59296e68faa8fc116171481a6d4943f49e8ed0
Ian Cantor <icantor@fandm.edu>
2018-01-27 15:01:46 -0500

    Mini timer countdown

--- main/main.gd
+++ main/main.gd
@@ -18,6 +18,8 @@ var display_label
 var input_container
 var input_options
 var mini_scene
+var mini_timer
+var mini_timer_label
 var timer
 var timer_label
 
@@ -26,6 +28,8 @@ func _ready():
 	display_label = get_node("GameDisplay/DisplayLabel")
 	input_container = get_node("InputsTexture/InputsContainer")
 	input_options = get_node("InputsTexture/InputsContainer").get_children()
+	mini_timer = get_node("MiniTimer")
+	mini_timer_label = get_node("MiniTimer/MiniTimerLabel")
 	timer = get_node("Timer")
 	timer_label = get_node("Timer/TimerLabel")
 	
@@ -40,8 +44,15 @@ func _process(delta):
 	
 	update_countdown(timer.time_left)
 	
-	if current_state == MINI && mini_scene.display != "":
-		display_label.set_text(mini_scene.display)
+	if current_state == MINI:
+		update_mini_countdown(mini_timer.time_left)
+
+		if mini_scene.display != "":
+			display_label.set_text(mini_scene.display)
+
+		if mini_timer.time_left == 0:
+			mini_timer.set_wait_time("5.0")
+			unload_mini_scene()
 
 func _input(event):
 	match current_state:
@@ -78,12 +89,20 @@ func load_mini_scene():
 	input_container.hide()
 	cursor.hide()
 
+	mini_timer.start()
+	mini_timer_label.show()
+
 func unload_mini_scene():
 	mini_scene.queue_free()
+	mini_timer_label.hide()
 	current_state = MAIN
 	input_container.show()
 	cursor.show()
 	
+func update_mini_countdown(seconds):
+	var display_text = String(seconds).split(".")[0]
+	mini_timer_label.set_text(display_text)
+
 func update_countdown(seconds):
 	var display_text = String(seconds).split(".")[0]
 	timer_label.set_text(display_text)
